01cbf9f35d0a9b0a980b3f14a5d736fef7ac9bf9
Cameron N Perry <cameron@pivotalaction.com>
2015-02-12 12:53:54 -0800

    Updated with a string tables export/import gotcha

--- LocalizationDemo/ViewController.swift
+++ LocalizationDemo/ViewController.swift
@@ -19,6 +19,15 @@ class ViewController: UIViewController {
         
     }
 
+    func tableGenerationExample() {
+        // Check the development language xliff export - although this table doesn't exist, the export
+        // process generates an entry for a generated_table.strings file. When translated and imported,
+        // a file named generated_table.strings will be created with "string_key" = "string_key";
+        // This occurs simply beccause NSLocalizedString is here, pointing to a specific table name
+        let localized = NSLocalizedString("string_key", tableName: "generated_table", bundle: NSBundle.mainBundle(), comment: "The table should be generated.");
+    }
+
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
